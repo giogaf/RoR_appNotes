8663f0fb152afc06c8adc0207d8fbfb2232c140a
asdasd <nuvokay@leeching.net>
2016-05-10 19:34:37 +0000

    docs controller agregate

---
+++ app/controllers/docs_controller.rb
@@ -0,0 +1,25 @@
+class DocsController < ApplicationController
+    def index
+    end
+    def new
+    end
+    def create
+    end
+    def show
+    end
+    
+    def edit
+    end
+    def update 
+    end
+    def destroy
+        
+    private
+        def find_doc
+        end
+        def     doc_params
+        end
+        
+    
+        
+end
